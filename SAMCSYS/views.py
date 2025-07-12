59997e2dde9b15edbc7d7b8b81d4742fc0b86de9
satocat845 <phonephaichitskd@gmail.com>
2025-07-12 16:07:37 +0700

    update params

--- SAMCSYS/views.py
+++ SAMCSYS/views.py
@@ -6450,6 +6450,13 @@ class FATransferLogsViewSet(viewsets.ModelViewSet):
     serializer_class = FATransferLogsSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        queryset = FA_Transfer_Logs.objects.all().order_by('transfer_id')
+        asset_list_id = self.request.query_params.get('asset_list_id')
+        if asset_list_id:
+            queryset = queryset.filter(asset_list_id=asset_list_id)
+        return queryset
+
     def perform_create(self, serializer):
         from django.db import transaction
         import logging
